b4dd3a8e1f98b8993b4cbd003171a5344b029901
Simon Shanks <sshanks@kx.com>
2020-05-17 17:09:22 +0100

    Add ldap_connect. Closes #5

--- src/kdbldap.h
+++ src/kdbldap.h
@@ -13,8 +13,16 @@
 extern "C" {
 #endif
 
+/**
+ * Initialise library with list of potential LDAP servers
+ * to use.
+ * 
+ * @param uris Symbol List type. List of connection URIs.
+ * e.g. ldap://127.0.0.1:389
+ */
+EXP K kdbldap_init(K uris);
+
 /* TODO */
-EXP K kdbldap_init(K todo);
 EXP K kdbldap_set_option(K global,K option,K value);
 
 #ifdef __cplusplus
