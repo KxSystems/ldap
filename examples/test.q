fa5019f5d99971413f6f3600c3ce1fb8f5e715c7
Simon Shanks <sshanks@kx.com>
2020-05-17 18:40:52 +0100

    Addition of int values for set_option. Ref #6

--- examples/test.q
+++ examples/test.q
@@ -2,4 +2,4 @@
 \c 25 2000
 
 .ldap.init[enlist `$"ldap://127.0.0.1:389"]
-.ldap.setOption[1b;17i;1i]
+.ldap.setOption[1b;`LDAP_OPT_PROTOCOL_VERSION;3]
--- src/kdbldap.c
+++ src/kdbldap.c
@@ -5,8 +5,16 @@
 #include <stdlib.h>
 
 #define CHECK_PARAM_TYPE(x,y,z) {if (x->t != y) return krr((S)"Function " #z " called with incorrect param type for " #x "");}
+#define CHECK_PARAM_INT_TYPE(x,z) {if (x->t != -KI && x->t != -KJ) return krr((S)"Function " #z " called with incorrect param type for " #x "");}
 
 static LDAP* LDAP_SESSION = NULL;
+
+static int getInt(K val)
+{
+    if (val->t==-KI)
+        return val->i;
+    return (int)val->j;
+}
 
 K kdbldap_init(K uris)
 {
@@ -29,13 +37,45 @@ K kdbldap_init(K uris)
     return ki(ldap_initialize( &LDAP_SESSION, csvUris));
 }
 
+static K setIntOption(LDAP* ld, int option, K value)
+{
+    CHECK_PARAM_INT_TYPE(value,"set_option");
+    int val = getInt(value);
+    return ki(ldap_set_option(ld, option, &val));
+} 
+
 K kdbldap_set_option(K global, K option,K value)
 {
     CHECK_PARAM_TYPE(global,-KB,"set_option");
-    CHECK_PARAM_TYPE(option,-KI,"set_option");
+    CHECK_PARAM_TYPE(option,-KS,"set_option");
     LDAP* ld = (global->g)?NULL:LDAP_SESSION;
-    /* TODO */
-    printf("LDAP_OPT_PROTOCOL_VERSION is %d\n",LDAP_OPT_PROTOCOL_VERSION);
-    // example - returnCode = ldap_set_option(ld, option->i, (void*)&version);
-    return ki(1);
+    
+    /* LDAP SUPPORTED OPTIONS */
+    if (strcmp(option->s,"LDAP_OPT_CONNECT_ASYNC")==0)
+        return setIntOption(ld,LDAP_OPT_CONNECT_ASYNC,value);
+    if (strcmp(option->s,"LDAP_OPT_DEBUG_LEVEL")==0)
+        return setIntOption(ld,LDAP_OPT_DEBUG_LEVEL,value);
+    if (strcmp(option->s,"LDAP_OPT_DEREF")==0)
+        return setIntOption(ld,LDAP_OPT_DEREF,value);
+    if (strcmp(option->s,"LDAP_OPT_PROTOCOL_VERSION")==0)
+        return setIntOption(ld,LDAP_OPT_PROTOCOL_VERSION,value);
+    if (strcmp(option->s,"LDAP_OPT_REFERRALS")==0)
+        return setIntOption(ld,LDAP_OPT_REFERRALS,value);
+    if (strcmp(option->s,"LDAP_OPT_RESULT_CODE")==0)
+        return setIntOption(ld,LDAP_OPT_RESULT_CODE,value);
+    if (strcmp(option->s,"LDAP_OPT_SIZELIMIT")==0)
+        return setIntOption(ld,LDAP_OPT_SIZELIMIT,value);
+    if (strcmp(option->s,"LDAP_OPT_TIMELIMIT")==0)
+        return setIntOption(ld,LDAP_OPT_TIMELIMIT,value);
+    /* TODO other ldap options */
+    /* SASL SUPPORTED OPTIONS - TODO */
+    /* TCP SUPPORTED OPTIONS */
+    if (strcmp(option->s,"LDAP_OPT_X_KEEPALIVE_IDLE")==0)
+        return setIntOption(ld,LDAP_OPT_X_KEEPALIVE_IDLE,value);
+    if (strcmp(option->s,"LDAP_OPT_X_KEEPALIVE_PROBES")==0)
+        return setIntOption(ld,LDAP_OPT_X_KEEPALIVE_PROBES,value);
+    if (strcmp(option->s,"LDAP_OPT_X_KEEPALIVE_INTERVAL")==0)
+        return setIntOption(ld,LDAP_OPT_X_KEEPALIVE_INTERVAL,value);
+    /* TLS SUPPORTED OPTIONS - TODO */
+    return krr("Unsupported option");
 }
