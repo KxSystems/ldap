0bac6a50be2fc5996cc96c524ae0bd9a8967b6b4
Simon Shanks <sshanks@kx.com>
2020-06-05 16:30:07 +0100

    Add apache license to install

--- docker_build.bat
+++ docker_build.bat
@@ -1,2 +1,2 @@
 docker build -f Dockerfile.build -t ldapdev .
-docker run --rm -it -v C:\Users\sshanks\Development\ldap\:/source/code ldapdev /bin/bash -c "cd build && cmake .. && make"
+docker run --rm -it -v C:\Users\sshanks\Development\ldap\:/source/code ldapdev /bin/bash -c "cd build && cmake .. && make install"
